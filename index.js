a6eb0b16e618909f606e6020348eab842e3d76f1
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-18 01:11:23 -0400

    commit tree 3bd5fc8110a70c5bd15762b137843a9347890b81

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2 
+let multiply = num1 * num2  
+
+let random = 1
+Math.random() * 10 + 1;
+
+let num3 = 4;
+let num4 = 1;
+let mod = num3 / num4;
+
+
+let max = 20
+Math.max(max);
